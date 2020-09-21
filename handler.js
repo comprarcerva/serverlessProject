719e6c0d73795f0773226a1548545294e587409e
vargascookbook <eduardo.vargas@gmail.com>
2020-09-21 11:01:23 -0300

    Primeiro Commit

---
+++ handler.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const dispatch = require('./dispatch');
+const userFavorites = require('./userFavorites/userFavorites');
+
+module.exports.intents = (event, context, callback) => {
+  try {
+    console.log(`event.bot.name=${event.bot.name}`);
+    dispatch(event).then(response => {
+      callback(null, response);
+    });
+  } catch (err) {
+    callback(err);
+  }
+};
+
+module.exports.saveUserFavorites = (event, context, callback) => {
+  console.log('saveUserFavorites lambda called');
+  console.log(event);
+
+  var item = event.Records.dynamodb.NewImage;
+  console.log(item);
+
+  userFavorites(item.userId.S, item.cervaTipo.S, item.cervejaTamanho.S);
+  callback(null, null);
+};
